f6192e71ecd17acf04186ee46fe1392c09f98959
hathuanihi <hathu20905@gmail.com>
2025-11-07 03:33:05 +0700

    fix: product hook

--- src/stores/selectors/product.selector.ts
+++ src/stores/selectors/product.selector.ts
@@ -1,7 +1,7 @@
 import { createSelector } from '@reduxjs/toolkit';
 import type { RootState } from '@/stores/store';
 
-const selectProductState = (state: RootState) => (state as any).product as ReturnType<any>;
+const selectProductState = (state: RootState) => state.product; 
 
 export const selectAllProducts = createSelector([selectProductState], (s) => s.products);
 export const selectSelectedProduct = createSelector([selectProductState], (s) => s.selectedProduct);
