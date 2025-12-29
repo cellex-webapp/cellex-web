e23923f9ab7e5f2b30941df17c1dfa145db5e9e7
hathuanihi <hathu20905@gmail.com>
2025-12-30 02:34:35 +0700

    fix detail ux

--- src/features/clients/pages/Product/ProductDetailPage.tsx
+++ src/features/clients/pages/Product/ProductDetailPage.tsx
@@ -1,9 +1,15 @@
+import { RecommendationSectionPreview } from "../../components/Recommendation";
 import ProductDetailCard from "./ProductDetailCard";
 
 const ProductDetailPage: React.FC = () => {
 	return (
 		<div className="container mx-auto py-6">
 			<ProductDetailCard />
+			<RecommendationSectionPreview 
+					title="Dành riêng cho bạn" 
+					maxItems={12}
+					viewAllLink="/recommendations"
+				/>
 		</div>
 	);
 };
